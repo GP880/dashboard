3ff2630d13c75dbf3672712f7264cf1bd2da31c4
Seu Nomgit config --global user.name Seu <george.pinheiro19@gmail.com>
2023-09-28 21:47:21 -0300

    teste

--- app.py
+++ app.py
@@ -1,1 +1,26 @@
-teste
+from flask import Flask
+import dash
+from dash import dcc, html
+import seaborn as sns
+import pandas as pd
+import plotly.express as px
+
+server = Flask(__name__)
+app = dash.Dash(__name__,
+                server=server,
+                routes_pathname_prefix='/')
+
+iris = sns.load_dataset('iris')
+fig = px.scatter(iris,
+                 x='sepal_length',
+                 y='sepal_width',
+                 color='species',
+                 title='Exemplo de Gráfico em Ciencia de Dados')
+
+app.layout = html.Div([
+    html.H1('Usando Dash em Flask'),
+    dcc.Graph(figure=fig)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
